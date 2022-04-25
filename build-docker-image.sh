390a64ff1d90b448773387cb63255b2d76eead10
Brian Gleeson <brian.gleeson@ie.ibm.com>
2022-04-25 15:29:43 +0100

    Hello containers master (#2357)
    * Update to beta
    
    * Fix pvc
    
    * remove volumeMounts
    
    * Fix clone steps
    
    * Test worksapce fix
    
    * Test workspace fix 2
    
    * Debug file system
    
    * Fix revision
    
    * Add defaults and more workspace fixes
    
    * Fix typos
    
    * Test build script fix
    
    * Script fix
    
    * Test script fix
    
    * Fix script
    
    * More script fixes
    
    * Test common script
    
    * scripting
    
    * scriptish
    
    * Its broken I give up

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -19,6 +19,7 @@ echo "GIT_BRANCH=${GIT_BRANCH}"
 echo "GIT_COMMIT=${GIT_COMMIT}"
 echo "DOCKER_ROOT=${DOCKER_ROOT}"
 echo "DOCKER_FILE=${DOCKER_FILE}"
+echo "API_KEY=${IBMCLOUD_API_KEY}"
 
 # View build properties
 if [ -f build.properties ]; then 
@@ -34,8 +35,8 @@ fi
 # To review or change build options use:
 # bx cr build --help
 
-echo -e "Existing images in registry"
-bx cr images
+# echo -e "Existing images in registry"
+# bx cr images
 
 # Minting image tag using format: BRANCH-BUILD_NUMBER-COMMIT_ID-TIMESTAMP
 # e.g. master-3-50da6912-20181123114435
@@ -48,14 +49,39 @@ if [ ! -z "${GIT_COMMIT}" ]; then
 fi
 IMAGE_TAG=${BUILD_NUMBER}-${IMAGE_TAG}
 if [ ! -z "${GIT_BRANCH}" ]; then IMAGE_TAG=${GIT_BRANCH}-${IMAGE_TAG} ; fi
+
+# Checking if buildctl is installed
+if which buildctl > /dev/null 2>&1; then
+  buildctl --version
+else 
+  echo "Installing Buildkit builctl"
+  curl -sL https://github.com/moby/buildkit/releases/download/v0.8.1/buildkit-v0.8.1.linux-amd64.tar.gz | tar -C /tmp -xz bin/buildctl && mv /tmp/bin/buildctl /usr/bin/buildctl && rmdir --ignore-fail-on-non-empty /tmp/bin
+  buildctl --version
+fi
+
+# Create the config.json file to make private container registry accessible
+export DOCKER_CONFIG=$(mktemp -d -t cr-config-XXXXXXXXXX)
+kubectl create secret --dry-run=true --output=json docker-registry registry-dockerconfig-secret --docker-server=${REGISTRY_URL} --docker-password=${API_KEY} --docker-username=iamapikey --docker-email=a@b.com | \
+jq -r '.data[".dockerconfigjson"]' | base64 -d > ${DOCKER_CONFIG}/config.json
+
 echo "=========================================================="
-echo -e "BUILDING CONTAINER IMAGE: ${IMAGE_NAME}:${IMAGE_TAG}"
+echo -e "Building container image: ${IMAGE_NAME}:${IMAGE_TAG}"
 if [ -z "${DOCKER_ROOT}" ]; then DOCKER_ROOT=. ; fi
 if [ -z "${DOCKER_FILE}" ]; then DOCKER_FILE=${DOCKER_ROOT}/Dockerfile ; fi
-set -x
-bx cr build -t ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG} ${DOCKER_ROOT} -f ${DOCKER_FILE}
-set +x
 
+# set -x
+# docker build -f ${DOCKER_ROOT}/${DOCKERFILE} -t ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG} ${DOCKER_ROOT}
+# docker push ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
+# set +x
+# bx cr image-inspect ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
+
+set -x
+buildctl build \
+    --frontend dockerfile.v0 --opt filename=${DOCKER_FILE} --local dockerfile=${DOCKER_ROOT} \
+    --local context=${DOCKER_ROOT} \
+    --import-cache type=registry,ref=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME} \
+    --output type=image,name="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}",push=true
+set +x
 bx cr image-inspect ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
 
 # Set PIPELINE_IMAGE_URL for subsequent jobs in stage (e.g. Vulnerability Advisor)
